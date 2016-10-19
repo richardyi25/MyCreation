bad30d6e1b880d03b26b3ca5620180f934f23e2a
Yi <Richard.Yi@student.tdsb.on.ca>
2016-10-19 15:02:27 -0400

    End of Wednesday period

---
+++ Background1.java
@@ -0,0 +1,29 @@
+/*
+Richard Yi
+Background #1
+Mrs. Krasteva
+Oct. 19 2016
+
+
+This program...
+*/
+
+import hsa.Console;
+import java.awt.*;
+
+public class Background1 
+{
+    private Console c;
+    
+    public Background1 (Console con)
+    {
+        c = con;
+        draw();
+    }
+
+    private void draw ()
+    {
+        Color sand = new Color(255, 233, 121);
+    }
+
+}
