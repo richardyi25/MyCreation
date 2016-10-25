1c1cbb1c0ee43b8402c6843ee6aeb51e58bc9129
Yi <richardyi25@gmail.com>
2016-10-25 15:06:30 -0400

    End of class Tuesday

--- Sanic.java
+++ Sanic.java
@@ -23,13 +23,35 @@ public class Sanic implements Runnable
     private void sanic ()
     {
 	Color sanicBody = new Color (2, 28, 217);
-	Color sanicSkin = new Color (33, 159, 195);
+	Color sanicSkin = new Color (255, 255, 179);
 	Color sanicShoes = new Color (237, 23, 0);
 
 	c.setColor (sanicBody);
+	c.fillOval (55, 35, 50, 40); //head
+	c.fillRect (70, 70, 20, 10); //neck
 	c.fillOval (55, 70, 50, 60); //body
-	
-	c.setColor(sanicSkin);
-	c.fillOval(70, 95, 10, 10); // "belly"
+
+
+	c.setColor (sanicSkin);
+	c.fillOval (70, 95, 20, 20); // "belly"
+	c.fillOval (65, 55, 30, 10); //mouth outline
+	c.fillOval (65, 45, 15, 7); //eye background
+	c.fillOval (80, 45, 15, 7); //eye background
+
+	c.setColor (Color.black);
+	c.fillOval (70, 45, 5, 7); //eyeballs
+	c.fillOval (85, 45, 5, 7);
+	c.drawLine (70, 60, 90, 60);
+
+	c.setColor (sanicBody);
+	int hair1X[] = {50, 90, 65};
+	int hair1Y[] = {10, 33, 30};
+	c.fillPolygon (hair1X, hair1Y, 3);
+
+	int hair2X[] = {};
+	int hair2Y[] = {};
+
+	int hair3X[] = {};
+	int hair3Y[] = {};
     }
 }
